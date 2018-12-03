3b66ac694ca3c320389e76de3e68ebaac6429f66
Jon Zachary <jontzachary@gmail.com>
2018-12-03 02:13:11 -0500

    intitial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+/** Create an array of arrays each of equal length that mirrors a matrix.
+@function makeGrid
+@param {Integer} size - Size of the grid.
+@returns {Array} Each element of the array will be an array of size size.
+*/
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
+  }
+};
