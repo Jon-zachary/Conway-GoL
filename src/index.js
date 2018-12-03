8401aa9a1f9c9c4a131657046465a4b25829998e
Jon Zachary <jontzachary@gmail.com>
2018-12-03 04:18:10 -0500

    add setGridVaue, getGridValue, pass tests

--- src/index.js
+++ src/index.js
@@ -12,10 +12,33 @@ const makeGrid = (n) => {
 }
 
 /**
-
+Set the value of an element in a grid.
+@function setGridValue
+@param {Integer} row - The row of the element to be set.
+@param {Integer} col - The column of the element to be set.
+@param {Boolean} val - The value of the element.
+@param {Array} grid - The grid to alter.
 */
+const setGridValue = (row, col, val, grid) => {
+  grid[row][col] = val;
+}
+
+/**
+Read the value of an element in a grid. If the value is out of bounds returns undefined.
+@function getGridValue
+@param {integer} row - The row of the element to be read.
+@param {integer} col - The column of the element to be read.
+@param {Array} grid - the grid to read.
+@returns {Boolean|undefined} Each element of a gid will be true or false or undefined.
+*/
+const getGridValue = (row, col, grid) => {
+  const val = (grid[row]) ? grid[row][col] : undefined;
+  return val;
+}
+
 
 module.exports = {
   makeGrid,
-
+  setGridValue,
+  getGridValue,
 }
--- tests/makeGrid.test.js
+++ tests/makeGrid.test.js
@@ -1,4 +1,4 @@
-const makeGrid = require("../src/index.js");
+const { makeGrid, setGridValue, getGridValue } = require("../src/index.js");
 
 describe("makeGrid", () => {
   test("makeGrid returns an array", () => {
@@ -17,5 +17,29 @@ describe("makeGrid", () => {
       expect(el).toBeInstanceOf(Array);
       expect(el).toHaveLength(2);
     });
+  });
+});
+
+describe("setGridValue", () => {
+  test("sets the grid value given coordinates", () => {
+    const grid = makeGrid(2);
+    setGridValue(0, 0, false, grid);
+    expect(grid[0][0]).toBe(false);
+  })
+})
+
+describe("getGridValue", () => {
+  test("returns grid val at coords", () => {
+    const grid = makeGrid(2);
+    setGridValue(0, 0, false, grid);
+    setGridValue(1, 1, true, grid);
+    expect(getGridValue(0, 0, grid)).toBe(false);
+    expect(getGridValue(1, 1, grid)).toBe(true);
+    expect(getGridValue(0, 1, grid)).toBe(undefined);
+  });
+  test("returns undefined if either index is out of range", () => {
+    const grid = makeGrid(2);
+    expect(getGridValue(2, 0, grid)).toBe(undefined);
+    expect(getGridValue(0, 2, grid)).toBe(undefined);
   });
 });
