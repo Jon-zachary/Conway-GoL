fac13dc73545cb74924afea2704173977f4413ee
Jon Zachary <jontzachary@gmail.com>
2018-12-11 21:31:50 -0500

    now.sh config

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const webpack = require("webpack");
 
-
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
@@ -10,5 +9,37 @@ module.exports = merge(common, {
     contentBase: "./dist",
     hot: true
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+          }
+        }
+      }
+    ]
+  }
 });
